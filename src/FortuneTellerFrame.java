567402d6dbccf033b4f9c081b892d4e35825cc4e
whsiq <staggsce@mail.uc.edu>
2023-02-21 15:22:52 -0500

    Added bottom panel to the frame

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FortuneTellerFrame extends JFrame {
 
@@ -25,18 +27,12 @@ public class FortuneTellerFrame extends JFrame {
     JButton quitBtn;
 
     ArrayList<String> fortunes = new ArrayList<>();
-//    fortunes.add("The man or woman you desire feels the same about you.");
-//    fortunes.add("Your shoes will make you happy today.");
-//    fortunes.add("A stranger, is a friend you have not spoken to yet.");
-//    fortunes.add("Our deeds determine us, as much as we determine our deeds.");
-//    fortunes.add("If winter comes, can spring be far behind?");
-//    fortunes.add("Jealousy doesn't open doors, it closes them!");
-//    fortunes.add("The man on the top of the mountain did not fall there.");
-//    fortunes.add("Nothing astonishes men so much as common sense and plain dealing.");
-//    fortunes.add("Land is always on the mind of a flying bird.");
-//    fortunes.add("Serious trouble will bypass you.");
-//    fortunes.add("You already know the answer to the questions lingering inside your head.");
-//    fortunes.add("The greatest risk is not taking one.");
+
+    Random rnd = new Random();
+
+    int newFortune;
+    int storedFortune;
+
     public FortuneTellerFrame() {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,6 +44,7 @@ public class FortuneTellerFrame extends JFrame {
 
         mainPnl.add(topPnl, BorderLayout.NORTH);
         mainPnl.add(midPnl, BorderLayout.CENTER);
+        mainPnl.add(lowPnl, BorderLayout.SOUTH);
 
     }
 
@@ -70,6 +67,51 @@ public class FortuneTellerFrame extends JFrame {
         scroller = new JScrollPane(fortuneTA);
 
         midPnl.add(scroller);
+    }
+
+    private void createBottomPanel() {
+        fortunes.add("The man or woman you desire feels the same about you.");
+        fortunes.add("Your shoes will make you happy today.");
+        fortunes.add("A stranger, is a friend you have not spoken to yet.");
+        fortunes.add("Our deeds determine us, as much as we determine our deeds.");
+        fortunes.add("If winter comes, can spring be far behind?");
+        fortunes.add("Jealousy doesn't open doors, it closes them!");
+        fortunes.add("The man on the top of the mountain did not fall there.");
+        fortunes.add("Nothing astonishes men so much as common sense and plain dealing.");
+        fortunes.add("Land is always on the mind of a flying bird.");
+        fortunes.add("Serious trouble will bypass you.");
+        fortunes.add("You already know the answer to the questions lingering inside your head.");
+        fortunes.add("The greatest risk is not taking one.");
+
+        lowPnl = new JPanel();
+        lowPnl.setLayout(new GridLayout(1,2));
+
+        fortuneBtn = new JButton("Read My Fortune!");
+        fortuneBtn.setFont(btnFont);
+        fortuneBtn.addActionListener((ActionEvent ae) -> {
+            newFortune = rnd.nextInt(11);
+
+            // Ensure that the same fortune isn't picked twice in a row
+
+            if (newFortune == storedFortune) {
+                while (newFortune == storedFortune) {
+                    newFortune = rnd.nextInt(11);
+                }
+                fortuneTA.append(fortunes.get(newFortune) + "\n");
+            }
+
+            else {
+                fortuneTA.append(fortunes.get(newFortune) + "\n");
+            }
+            storedFortune = newFortune;
+        });
+
+        quitBtn = new JButton("Quit");
+        quitBtn.setFont(btnFont);
+        quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
+
+        lowPnl.add(fortuneBtn);
+        lowPnl.add(quitBtn);
     }
 
 }
