70c3b636b7d81cdbaac79f968b7dd63f137d54d9
whsiq <staggsce@mail.uc.edu>
2023-02-21 14:30:52 -0500

    Initialized JFrame elements

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class FortuneTellerFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class FortuneTellerFrame extends JFrame {
+
+    // Initializations
+
+    JPanel mainPnl;
+    JPanel topPnl;
+    JPanel midPnl;
+    JPanel lowPnl;
+
+    JLabel titleLbl;
+    ImageIcon imgIcon;
+
+    Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, 48);
+    Font fortuneFont = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
+    Font btnFont = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
+
+    JTextArea fortuneTA;
+    JScrollPane scroller;
+
+    JButton fortuneBtn;
+    JButton quitBtn;
+
+
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,4 +1,4 @@
-public class Main {
+public class FortuneTellerViewer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
